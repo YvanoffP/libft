1e0af6775c2f0fdc504ccdf80714263720f10b99
YvanoffP <petruzzi.yvan@gmail.com>
2021-09-16 13:29:23 +0200

    Updated .h file and added isalpha.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-void *memccpy(void *dest, const void *src, int c, size_t n)
+void *ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	int count;
 
--- libft.h
+++ libft.h
@@ -4,9 +4,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-size_t ft_strlen(char *s);
+size_t  ft_strlen(char *s);
+size_t  ft_strlcpy(char *dst, char *src, size_t size);
 void    ft_bzero(void *s, size_t n);
 void    *ft_memset(void *s, int c, size_t n);
 void    *ft_memcpy(void *dest, const void *src, size_t n);
+void    *ft_memccpy(void *dest, const void *src, int c, size_t n);
 
+int     isdigit(int c);
+int     isalpha(int c);
 #endif
