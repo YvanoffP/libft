f01cc5e6109c0290108114af874ff3cdbf808353
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-10-11 14:49:39 +0200

    Norminetted and check

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 14:46:04 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/10/11 14:48:46 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
@@ -5,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	count;
 
 	count = 0;
-	while (count >= n)
+	while (count < n)
 	{
 		((char *)dest)[count] = ((char *)src)[count];
 		count++;
