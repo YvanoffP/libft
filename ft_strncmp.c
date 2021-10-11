2fd2b58fd62f66d5c3f88e8e25959050bd6a3a17
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-10-11 17:45:27 +0200

    strncmp ok

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,16 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 17:12:55 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/10/11 17:39:12 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	count;
+int	ft_strncmp(const char	*s1, const char	*s2, size_t n)
+{	
+	unsigned int	count;
 
 	count = 0;
-	if (n == 0)
-		return (0);
-	while (s1[count] == s2[count] && s1[count] != '\0' && count < n)
+	while ((*(s1 + count) != '\0' || *(s2 + count) != '\0') && count < n)
+	{
+		if (! (*(s1 + count) == *(s2 + count)))
+			return (*(s1 + count) - *(s2 + count));
 		count++;
-	if (s1[count] == s2[count])
-		return (0);
-	else
-		return (s1[count] - s2[count]);
+	}
+	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 17:11:10 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/10/11 17:12:15 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
@@ -10,7 +22,7 @@ char	*ft_strrchr(const char *s, int c)
 		while (s[pos] != (char)c && pos > 0)
 			pos--;
 		if (s[pos] == (char)c)
-			return ((char *) (s + pos));
+			return ((char *)(s + pos));
 	}
 	return (NULL);
 }
