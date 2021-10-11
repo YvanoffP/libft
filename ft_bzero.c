034883c97b2863ef7af7a58d01c27bfd106d9c3c
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-10-11 14:15:10 +0200

    tester ok and normenetted

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 14:12:50 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/10/11 14:14:47 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
@@ -5,7 +17,7 @@ void	ft_bzero(void *s, size_t n)
 	size_t	count;
 
 	count = 0;
-	while (count <= n)
+	while (count < n)
 	{
 		((char *)s)[count] = '\0';
 		count++;
