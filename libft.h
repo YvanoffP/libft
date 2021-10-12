1f28ee4e969813121c559c4141e3a432874bb4f7
YvanoffP <petruzzi.yvan@gmail.com>
2021-10-12 04:25:37 +0200

    added all fd functions and updated header

--- libft.h
+++ libft.h
@@ -29,4 +29,9 @@ int     ft_atoi(const char *str);
 int     ft_isdigit(int c);
 int     ft_isalpha(int c);
 char	**ft_split(char const   *s, char	*c);
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd)
+void	ft_putnbr_fd(int n, int fd)
 #endif
