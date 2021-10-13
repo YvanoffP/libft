ff30cfc1c18c6cfed8b567ae4dea5c409593355c
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-10-14 00:33:43 +0200

    small tuning

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 17:11:10 by ypetruzz          #+#    #+#             */
-/*   Updated: 2021/10/11 17:12:15 by ypetruzz         ###   ########.fr       */
+/*   Updated: 2021/10/14 00:32:49 by ypetruzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strrchr(const char *s, int c)
 	int	pos;
 
 	pos = ft_strlen(s);
+	if (s[0] == '\0')
+		return ((char *)s);
 	if (pos > 0)
 	{
 		while (s[pos] != (char)c && pos > 0)
