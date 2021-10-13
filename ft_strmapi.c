bc7ca57b7c6e58efdfd8dc39c8aa381421964f53
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-10-13 18:11:40 +0200

    working as intended

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 16:14:19 by ypetruzz          #+#    #+#             */
-/*   Updated: 2021/10/13 16:14:55 by ypetruzz         ###   ########.fr       */
+/*   Updated: 2021/10/13 18:11:16 by ypetruzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char	*result;
-	int		index;
+	char			*result;
+	unsigned int	index;
 
-	if (!s)
-		return (NULL);
+	if (!(ft_strncmp(s, "", 1)))
+		return (ft_strdup(""));
+	if (!f)
+		return (ft_strdup(s));
 	result = malloc(sizeof(char) * ft_strlen((char *)s));
 	if (!result)
 		return (NULL);
 	index = 0;
 	while (s[index])
 	{
-		result[index] = f(index, s[index]);
+		result[index] = (*f)(index, s[index]);
 		index++;
 	}
 	return (result);
