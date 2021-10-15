070a06456d0dae6e3d9ccfb1a8f53112d9f41414
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-10-15 16:46:23 +0200

    small tweaks for security

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 16:14:19 by ypetruzz          #+#    #+#             */
-/*   Updated: 2021/10/15 03:24:00 by ypetruzz         ###   ########.fr       */
+/*   Updated: 2021/10/15 16:45:23 by ypetruzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*result;
 	unsigned int	index;
 
+	if (!s || !f)
+		return (NULL);
 	if (!(ft_strncmp(s, "", 1)))
 		return (ft_strdup(""));
-	if (!s)
-		return (NULL);
-	if (!f)
-		return (ft_strdup(s));
-	result = malloc(sizeof(char) * ft_strlen((char *)s) + 1);
+	result = malloc(sizeof(char) * ft_strlen(s) + 1);
 	if (!result)
 		return (NULL);
 	index = 0;
